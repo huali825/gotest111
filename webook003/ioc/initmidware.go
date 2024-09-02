925024ea3e005293b009e9adb11bc77b35dc3f5e
huali0809 <1132457928@qq.com>
2024-09-02 17:05:16 +0800

    middleWare的使用

--- webook003/ioc/initmidware.go
+++ webook003/ioc/initmidware.go
@@ -7,19 +7,26 @@ import (
 	"time"
 )
 
-func InitMiddleware(server *gin.Engine) gin.IRoutes {
-	return server.Use(cors.New(cors.Config{
-		//AllowOrigins:     []string{"*"},
-		AllowMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"},
-		AllowHeaders: []string{"Origin", "Content-Type", "Authorization"},
-		//ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
-		AllowOriginFunc: func(origin string) bool {
-			if strings.HasPrefix(origin, "http://localhost") {
-				return true
-			}
-			return strings.Contains(origin, "localhost")
+func InitMiddleware(server *gin.Engine) *gin.Engine {
+	server.Use(
+		cors.New(cors.Config{
+			//AllowOrigins:     []string{"*"},
+			AllowMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"},
+			AllowHeaders: []string{"Origin", "Content-Type", "Authorization"},
+			//ExposeHeaders:    []string{"Content-Length"},
+			AllowCredentials: true,
+			MaxAge:           12 * time.Hour,
+			AllowOriginFunc: func(origin string) bool {
+				if strings.HasPrefix(origin, "http://localhost") {
+					return true
+				}
+				return strings.Contains(origin, "localhost")
+			},
+		}),
+		func(ctx *gin.Context) {
+			println("这是我的 Middleware")
 		},
-	}))
+	)
+
+	return server
 }
