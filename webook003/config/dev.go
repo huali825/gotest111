d14e19e960fdc3f2cfa2410724c5d539398cd1a1
huali0809 <1132457928@qq.com>
2024-09-21 22:30:38 +0800

    k8s的pvc在docker部署失败

--- webook003/config/dev.go
+++ webook003/config/dev.go
@@ -4,7 +4,7 @@ package config
 
 var Config = config{
 	DB: DBConfig{
-		DSN: "root:root@tcp(localhost:13316)/webook",
+		DSN: "root:root@tcp(localhost:30002)/webook",
 	},
 	Redis: RedisConfig{
 		Addr: "localhost:6379",
